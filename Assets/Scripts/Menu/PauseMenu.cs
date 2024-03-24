474a1bb47290fcdc46e4d9cf4198505ba604ca68
Faiza <faiza.fatema.1947@gmail.com>
2024-03-24 15:03:03 +0530

    I fixed the pause menu

--- Assets/Scripts/Menu/PauseMenu.cs
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -8,29 +8,13 @@ public class PauseMenu : MonoBehaviour
     public static bool GameIsPaused = false;
     public GameObject pauseMenuUI;
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (GameIsPaused)
-            {
-           
-                Resume();
-            }
-            else
-            {
-               
-                Pause();
-            }
-        }
-    }
     public void Resume()
     {
         pauseMenuUI.SetActive(false);
         GameIsPaused = false;
         Time.timeScale = 1f;
     }
-    void Pause()
+    public void Pause()
     {
         pauseMenuUI.SetActive(true);
         GameIsPaused = true;
